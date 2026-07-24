1269234aa9b915c3dd82912cc676c82c9d1d86c1
mahmoudfahmy616572 <mahmoudfahmy7776@gmail.com>
2026-07-24 21:20:43 +0300

    fix(web): cap Sizer to 480x960 max so driver screens scale correctly on web

--- lib/core/utils/sizer.dart
+++ lib/core/utils/sizer.dart
@@ -4,6 +4,12 @@ class Sizer {
   static late double width;
   static late double height;
 
+  static const double _maxDesignWidth = 480;
+  static const double _maxDesignHeight = 960;
+
+  static double get safeWidth => width > _maxDesignWidth ? _maxDesignWidth : width;
+  static double get safeHeight => height > _maxDesignHeight ? _maxDesignHeight : height;
+
   static void init(BuildContext context) {
     width = MediaQuery.of(context).size.width;
     height = MediaQuery.of(context).size.height;
@@ -11,8 +17,8 @@ class Sizer {
 }
 
 extension NumExtension on num {
-  double get h => this / 812 * Sizer.height;
-  double get w => this / 375 * Sizer.width;
-  double get sp => this / 375 * Sizer.width;
-  double get r => this / 375 * Sizer.width;
+  double get h => this / 812 * Sizer.safeHeight;
+  double get w => this / 375 * Sizer.safeWidth;
+  double get sp => this / 375 * Sizer.safeWidth;
+  double get r => this / 375 * Sizer.safeWidth;
 }
